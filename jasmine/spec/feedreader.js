8d69bd3c1b175d00bf5167796120b7ba48aecc15
brutal-kitten <olgaprudyus@gmail.com>
2018-05-13 12:00:05 +0200

    test: finish the test siut 'RSS Feeds'

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,12 +31,23 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it('every object has a URL defined and the URL is not empty ', function() {
+            for(let i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].url.length).not.toBe(0);
+            };
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('every object has a name defined and the name is not empty', function() {
+            for(let i = 0; i < allFeeds.length; i++) {
+                  expect(allFeeds[i].name).toBeDefined();
+                  expect(allFeeds[i].name.length).not.toBe(0);
+            }
+         });
     });
 
 
