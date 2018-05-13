ea30c625271ed5b3aee2ea621abb32e6e939a11e
brutal-kitten <olgaprudyus@gmail.com>
2018-05-13 14:48:45 +0200

    test: add a test suit 'Initial Entries'

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -79,6 +79,7 @@ $(function() {
           });
     });
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -86,7 +87,18 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         beforeEach(function(done) {
+             loadFeed(0, function() {
+                 done();
+             });
+         });
 
+         it('there is at least a single .entry element within the .feed container.', function(done) {
+           const feed = document.querySelector('.feed');
+           expect(feed.hasChildNodes()).toBe(true);
+           done();
+         });
+    });
     /* TODO: Write a new test suite named "New Feed Selection" */
 
         /* TODO: Write a test that ensures when a new feed is loaded
